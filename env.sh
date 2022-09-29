cfd57efca39d67ddb1d104fa3c193b5aaee7b29b
Alexandre Teles <alexandre.teles@ufba.br>
2022-09-29 02:01:25 -0300

    change extensions and add env setup file

---
+++ env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script is used to setup the environment variables
+
+export GITHUB_TOKEN=your_token
+export UVICORN_HOST=0.0.0.0
+export UVICORN_PORT=8000
+export UVICORN_LOG_LEVEL=debug
+export REDIS_URL=127.0.0.1
+export REDIS_PORT=6379
+export SENTRY_DSN=your_sentry_dsn
